b287de0a2a370ab08b9cb13e9eacb0cb2ba4535a
Magesh <tkmagesh77@gmail.com>
2019-01-14 14:14:52 +0800

    Calculator implementation

--- 01-lang/src/calculator.js
+++ 01-lang/src/calculator.js
@@ -1,3 +1,8 @@
-function add(x,y){
-	return x + y;
+function add(){
+	function parseArg(n){
+		if (Array.isArray(n)) return add.apply(this, n);
+		if (typeof n === 'function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n,10);
+	}
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
