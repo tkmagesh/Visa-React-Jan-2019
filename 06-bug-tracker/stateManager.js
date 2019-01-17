9eb14682c41159e6bea6ecb98cb4cca4c298e1a0
Magesh <tkmagesh77@gmail.com>
2019-01-17 14:36:32 +0800

    using stateManager in bugTracker - adding new bugs - ui not rendering

---
+++ 06-bug-tracker/stateManager.js
@@ -0,0 +1,47 @@
+var SM = (function(){
+	var _currentState = undefined,
+		_subscribers = [],
+		_reducer = function(){},
+		_init_action = '@@INIT_ACTION';
+
+	function getState(){
+		return _currentState;
+	}
+
+	function subscribe(listenerFn){
+		if (typeof listenerFn === 'function')
+			_subscribers.push(listenerFn);
+	}
+
+	function triggerChange(){
+		_subscribers.forEach(listenerFn => listenerFn());
+	}
+
+	function dispatch(action){
+		var newState = _reducer(_currentState, action);
+		if (newState === _currentState) return;
+		_currentState = newState;
+		triggerChange();
+	}
+
+	function createStore(reducer){
+		if (typeof reducer !== 'function') 
+			throw new Error('a reducer is mandatory to create a store');
+		_reducer = reducer;
+		_currentState = _reducer(_currentState, _init_action);
+		return { getState, subscribe, dispatch };
+	}
+
+	function bindActionCreators(actionCreators, dispatch){
+		let result = {};
+		for(let key in actionCreators){
+			result[key] = function(){
+				let action = actionCreators[key].apply(undefined, arguments);
+				dispatch(action);
+			}
+		}
+		return result;
+	}
+
+	return { createStore, bindActionCreators };
+})();
