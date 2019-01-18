e487ab7458ef0ec37bb18480aea58979ba5a71f7
Magesh <tkmagesh77@gmail.com>
2019-01-18 14:30:48 +0800

    BugItem as a higher order component

--- 08-bug-tracker-app/src/bugTracker/views/bugItem.js
+++ 08-bug-tracker-app/src/bugTracker/views/bugItem.js
@@ -1,21 +1,57 @@
+
+
+/*
 import React, { Component } from 'react';
-
 class BugItem extends Component{
-
-	onBugNameClick = () => {
-		this.props.toggle(this.props.bug);
-	}
 	render(){
-		let { bug } = this.props;
+		let { bug, toggle } = this.props;
 		let bugStyle = 'bugname ' + (bug.isClosed ? 'closed' : '');
 		return(
 			<li>
-				<span className={bugStyle} onClick={this.onBugNameClick}>
+				<span className={bugStyle} onClick={() => toggle(bug)}>
 					{JSON.stringify(bug)}
 				</span>
 			</li>	
 		);
 	}
 }
+*/
+
+import React from 'react';
+
+//Higher Order Components
+
+
+/*let BugItem = function(props){
+	let { bug, toggle } = props
+	let bugStyle = 'bugname ' + (bug.isClosed ? 'closed' : '');
+	return(
+		<li>
+			<span className={bugStyle} onClick={() => toggle(bug)}>
+				{JSON.stringify(bug)}
+			</span>
+		</li>	
+	);
+}*/
+
+/*let BugItem = function({ bug, toggle }){
+	return(
+		<li>
+			<span className={'bugname ' + (bug.isClosed ? 'closed' : '')} onClick={() => toggle(bug)}>
+				{JSON.stringify(bug)}
+			</span>
+		</li>	
+	);
+}*/
+
+
+let BugItem = ({ bug, toggle }) => (
+	<li>
+		<span className={'bugname ' + (bug.isClosed ? 'closed' : '')} onClick={() => toggle(bug)}>
+			{JSON.stringify(bug)}
+		</span>
+	</li>	
+);
+
 
 export default BugItem;
